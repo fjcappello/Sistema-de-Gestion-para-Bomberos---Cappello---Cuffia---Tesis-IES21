5221d4db8d4f7d08c0d130d401e983528f5f6f41
Facundo Cuffia <facucuffia@gmail.com>
2025-04-22 19:49:32 -0300

    Implementacion de hash en passwords

--- backend/Controllers/login.controller.js
+++ backend/Controllers/login.controller.js
@@ -1,52 +1,76 @@
 const db = require('../DB/db.js');
+const bcryptjs = require('bcryptjs');
 const { registrarLog } = require('../Middlewares/logSeguridadLogger.js');
 
-const loginUsuario = (req, res) => {
+const loginUsuario = async function loginUsuario(req, res){
+  let valida = false;
   const { legajo, password } = req.body;
   const query = `
-    SELECT CONCAT(p.nombre, ' ', p.apellido) AS nombre_completo, l.primer_ingreso
+    SELECT CONCAT(p.nombre, ' ', p.apellido) AS nombre_completo, l.primer_ingreso, l.contraseña AS clave
     FROM personal p
     INNER JOIN login l ON p.legajo = l.legajo
-    WHERE p.legajo = ? AND l.contraseña = ?
+    WHERE p.legajo = ? ;
   `;
-
-  db.query(query, [legajo, password], async (err, results) => {
+ //AND p.activo = 1
+  db.query(query, [legajo], async (err, results) => {
     if (err) {
       console.error('Error en el servidor al intentar iniciar sesión:', err);
       res.status(500).json({ success: false, error: 'Error en el servidor' });
-    } else if (results.length > 0) {
-      const { nombre_completo, primer_ingreso } = results[0];
-
-      // Registrar intento de inicio exitoso
-      registrarLog(
-        legajo,
-        `El usuario ${nombre_completo} inició sesión correctamente.`
-      );
-
-      res.json({ success: true, nombreCompleto: nombre_completo, primerIngreso: primer_ingreso });
-    } else {
-      res.json({ success: false, error: 'Legajo o contraseña incorrectos' });
     }
+    //VALIDACIÓN DE SI LA CONSULTAA DIO RESULTADOS 
+    else if (results.length > 0) {
+      const { nombre_completo, primer_ingreso, clave} = results[0];
+      //Si es primer ingreso hago la comparacion normal, sino la de bcryptjs
+      if(primer_ingreso == 0){
+        valida = await bcryptjs.compare(password, clave);
+      }
+      else if(password === clave){
+        valida = true;
+      }
+      //VALIDACION DEL ESTADO DEL PASS
+      if(valida){
+        // Registrar intento de inicio exitoso
+        registrarLog(
+          legajo,
+          `El usuario ${nombre_completo} inició sesión correctamente.`
+        );
+        return res.json({ success: true, nombreCompleto: nombre_completo, primerIngreso: primer_ingreso });
+      }
+      else{
+        return res.json({ success: false, error: 'Legajo o contraseña incorrectos' });
+      }
+    }
+    else{
+      return res.json({ success: false, error: 'Legajo o contraseña incorrectos' });
+    } 
   });
 };
 
-const cambiarPassword = (req, res) => {
+//DOC CONVERTI ESTO A ASYNC
+const cambiarPassword = async function cambiarPassword(req, res){
   const { legajo, nuevaPassword } = req.body;
   const query = `
     UPDATE login SET contraseña = ?, primer_ingreso = false WHERE legajo = ?
   `;
 
-  db.query(query, [nuevaPassword, legajo], async (err, result) => {
+  if (!legajo || !nuevaPassword) {
+    return res.status(400).json({ success: false, error: 'Faltan datos: legajo y password son requeridos' });
+  }
+
+  const hash = await bcryptjs.hash(nuevaPassword, 10);
+
+  db.query(query, [hash, legajo], async (err, result) => {
     if (err) {
       console.error('Error al cambiar la contraseña:', err);
       res.status(500).json({ success: false, error: 'Error al cambiar la contraseña' });
-    } else {
+    } 
+    else {
       registrarLog(
         legajo,
-        `Cambio de contraseña: El usuario ${legajo} cambió su contraseña y desactivó el primer ingreso.`
+        `Cambio de contraseña: El usuario ${legajo} cambió su contraseña`
       );
 
-      res.json({ success: true, message: 'Contraseña actualizada correctamente' });
+      res.status(202).json({ success: true, message: 'Contraseña actualizada correctamente' });
     }
   });
 };
