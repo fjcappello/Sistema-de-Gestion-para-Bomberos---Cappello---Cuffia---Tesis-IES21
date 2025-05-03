cfdee9688fc9556fae7a3df3b8dd59a8a42eb4e3
Franco Cappello <fjcappello@live.com>
2025-05-03 12:29:31 +0200

    Fix logSeguridad logout

--- backend/Routes/login.js
+++ backend/Routes/login.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { loginUsuario, cambiarPassword } = require('../Controllers/login.controller.js');
+const { loginUsuario, cambiarPassword, logoutUsuario } = require('../Controllers/login.controller.js');
 
 // Verifica si el legajo y la contraseña son correctos
 router.post('/login', loginUsuario);
 
 // Cambia la contraseña y actualiza el estado de primer ingreso
 router.post('/cambiar-password', cambiarPassword);
+
+// Registra el cierre de sesión
+router.post('/logout', logoutUsuario);
 
 module.exports = router;
